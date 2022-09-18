3c7081e9249c78a5774e4d5e5b541f7d57209e55
eriksnc <eriknajercor@gmail.com>
2022-09-17 19:50:28 -0500

    Se anexo la primera funcion con JS

---
+++ Tareas/js/funciones.js
@@ -0,0 +1,15 @@
+//Esta es una función
+function despedida(){
+    alert("Adios popo");
+}
+
+console.log("Hola desde la consola :3");
+
+console.log("Este es un texto")
+
+console.log(20)
+console.log(2.10)
+console.log(2,40)
+console.log(2+20)
+console.log(true)
+console.log(false)
